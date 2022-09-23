e98d7ef737f11dd419a80352d1f342bf73853b16
LENOVO <frisli.089.df@gmail.com>
2022-09-23 13:38:34 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int first = 22;
+        System.out.println("Значение переменной first с типом int равно " + first);
+        byte second = 127;
+        System.out.println("Значение переменной second с типом byte равно " + second);
+        short third = 23561;
+        System.out.println("Значение переменной third с типом short равно " + third);
+        long forth = 7584929;
+        System.out.println("Значение переменной forth с типом long равно " + forth);
+        float fifth = 7.7f;
+        System.out.println("Значение переменной fifth с типом float равно " + fifth);
+        double sixth = 9.1;
+        System.out.println("Значение переменной sixth с типом double равно " + sixth);
     }
 }
