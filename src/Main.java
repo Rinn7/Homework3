94367854c99e8eae61669a94a5fa71b5a0b3aea2
LENOVO <frisli.089.df@gmail.com>
2022-09-25 21:21:27 +0300

    Четвертое задание

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        var teacherA = 23;
-        System.out.println("У Людмилы Павловны " +teacherA+ " ученика.");
-        var teacherB = 27;
-        System.out.println("У Анны Сергеевны " +teacherB+ " ученика.");
-        var teacherC = 30;
-        System.out.println("У Екатерины Андреевны " +teacherC+ " учеников.");
-        var paper = 480;
-        System.out.println("Всего было закуплено " +paper+ " листов.");
-        var totalStudents = teacherA + teacherB + teacherC;
-        System.out.println("Всего учеников " +totalStudents+ " человек.");
-        var countOfPaper = paper / totalStudents;
-        System.out.println("На каждого ученика рассчитано " +countOfPaper+ " листов бумаги.");
+        short bottleCount = 16;
+        short time1 = 2;
+        System.out.println("Машина за " +time1+ " минуты изготавливает " +bottleCount+ " бутылок.");
+        byte time2 = 20;
+        int workA = ( bottleCount * time2 ) / time1;
+        System.out.println("За " +time2+ " минут машина изготавливает " +workA+ " бутылок.");
+        short time3 = 1440;
+        int workB = ( bottleCount * time3 ) / time1;
+        System.out.println("За " +time3+ " минут(сутки) машина изготавливает " +workB+ " бутылок.");
+        short time4 = 4320;
+        int workC = ( bottleCount * time4 ) / time1;
+        System.out.println("За " +time4+ " минут(3 дня) машина изготавливает " +workC+ " бутылок.");
+        int time5 = 43200;
+        int workD = ( bottleCount * time5 ) / time1;
+        System.out.println("За " +time5+ " минут(1 месяц) машина изготавливает " +workD+ " бутылок.");
     }
 }
