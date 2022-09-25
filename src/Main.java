cd3c26e0f6b06b7a4e9b97cb99ca33f5eda406b9
LENOVO <frisli.089.df@gmail.com>
2022-09-25 13:18:28 +0300

    Исправленное третье задание

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        var teacherA = 23;
+        byte teacherA = 23;
         System.out.println("У Людмилы Павловны " +teacherA+ " ученика.");
-        var teacherB = 27;
+        byte teacherB = 27;
         System.out.println("У Анны Сергеевны " +teacherB+ " ученика.");
-        var teacherC = 30;
+        byte teacherC = 30;
         System.out.println("У Екатерины Андреевны " +teacherC+ " учеников.");
-        var paper = 480;
+        short paper = 480;
         System.out.println("Всего было закуплено " +paper+ " листов.");
-        var totalStudents = teacherA + teacherB + teacherC;
+        byte totalStudents = (byte) (teacherA + teacherB + teacherC);
         System.out.println("Всего учеников " +totalStudents+ " человек.");
-        var countOfPaper = paper / totalStudents;
+        short countOfPaper = (short) (paper / totalStudents);
         System.out.println("На каждого ученика рассчитано " +countOfPaper+ " листов бумаги.");
     }
 }
